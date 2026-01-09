eaf01967cedb918f4a355b8d527358d4fcec8c34
blagojcho-juca <blagojcho.juca@docusketch.com>
2026-01-09 09:35:23 +0100

    removed jitpack implementation

--- kotlin-multiplatform/build.gradle.kts
+++ kotlin-multiplatform/build.gradle.kts
@@ -31,7 +31,7 @@ dependencies {
     dokkaPlugin("org.jetbrains.dokka:versioning-plugin:2.0.0")
 }
 
-val thetaClientVersion = "1.13.5"
+val thetaClientVersion = "1.13.9"
 group = "com.ricoh360.thetaclient"
 version = thetaClientVersion
 
